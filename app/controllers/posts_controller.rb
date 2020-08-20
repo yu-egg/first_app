91d611b6a5da746ff4e6ef3648f8131e509264bd
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-08-20 15:12:46 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  def index  #アクションを定義した
+    @posts = Post.all
+  end 
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
